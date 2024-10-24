396d5b49122c070bc7c2415d713cb68cb74f84b4
User <gigi.khomeriki@makingscience.com>
2024-10-24 15:57:50 +0400

    get queryResult log

--- src/multiple_value/multiple_value_container.js
+++ src/multiple_value/multiple_value_container.js
@@ -24,6 +24,7 @@ looker.plugins.visualizations.add({
     this.clearErrors();
     addBaseTagToHeadElement(document);
 
+    console.log(queryResponse, 'queryResponse');
     const measures = [].concat(
       queryResponse.fields.dimensions,
       queryResponse.fields.measures,
